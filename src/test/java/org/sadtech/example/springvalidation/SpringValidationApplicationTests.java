f4c0062d155057d3136bb7ee8eee1b5865c50054
uPagge <git@upagge.ru>
2021-03-22 15:40:33 +0300

    git init

---
+++ src/test/java/org/sadtech/example/springvalidation/SpringValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sadtech.example.springvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
